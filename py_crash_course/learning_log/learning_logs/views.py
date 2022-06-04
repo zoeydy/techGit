d8972afafd6db43b6476f00aa3ca742d0cad461e
zoeydy <zoey_dy@163.com>
2022-06-04 10:49:41 +0800

    add

---
+++ py_crash_course/learning_log/learning_logs/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+
+from .models import Topic
+from .forms import TopicForm
+
+# Create your views here.
+
+def index(request):
+    """ The home page for Learning log."""
+    return render(request, 'learning_logs/index.html')
+    
+def topics(request):
+    """ Show all topics """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ Show a single topic and all its entries. """
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """ add a new topic """
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+    
