b480e9a0df4df27b84a39da993d168f88b0cb73f
zoeydy <zoey_dy@163.com>
2022-06-07 18:52:32 +0800

    add something

--- py_crash_course/learning_log/learning_log/settings.py
+++ py_crash_course/learning_log/learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps 
     'learning_logs',
+    'users',
 
     # Default django apps.
     'django.contrib.admin',
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_URL = 'users:login'
